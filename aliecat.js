a2e41b72fead70d7fa5945318092d595226c4ca1
AlieCat <aliee91@gmail.com>
2016-02-19 17:27:58 -0600

    add semicolon

--- aliecat.js
+++ aliecat.js
@@ -1,7 +1,8 @@
 function flip() {
-		var c = document.getElementById("coin");
-    var p = document.getElementById("who")
-		c.classList.toggle("flipped");
+    var c = document.getElementById("coin");
+    var p = document.getElementById("who");
+    
+    c.classList.toggle("flipped");
     if (c.classList.contains('flipped')) {
     p.innerHTML = "I swear she's really working on stuff!";
     }
